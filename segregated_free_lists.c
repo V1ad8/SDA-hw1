c75ba7314ea7909a27ea474a2f71d7347a4e8925
V1ad8 <2004uvm@gmail.com>
2024-03-22 08:54:52 +0200

    Made simple FREE function

--- segregated_free_lists.c
+++ segregated_free_lists.c
@@ -33,11 +33,15 @@ typedef struct ll_list_t {
 } ll_list_t;
 
 // Function to initialize the heap
+//
 // Parameters:
 //	 - heap_start: The starting address of the heap
 //	 - num_lists: The number of segregated free lists
 //	 - bytes_per_list: The number of bytes per list
 //	 - data: Pointer to store the allocated memory for the heap
+//
+// Returns:
+//	 - The array of segregated free lists
 sfl_list_t *init_heap(size_t heap_start, size_t num_lists,
 		      size_t bytes_per_list, void **data)
 {
@@ -98,6 +102,7 @@ sfl_list_t *init_heap(size_t heap_start, size_t num_lists,
 }
 
 // Function to allocate memory using segregated free lists
+//
 // Parameters:
 //   - size: The size of the memory to allocate
 //   - lists: Pointer to the array of segregated free lists
@@ -234,7 +239,125 @@ void malloc_f(size_t size, sfl_list_t **lists, size_t *num_lists,
 	printf("Out of memory\n");
 }
 
+// Function to free memory using segregated free lists
+//
+// Parameters:
+//   - address: The address of the block to be freed
+//   - allocated_blocks: Pointer to the linked list of allocated blocks
+//   - lists: Pointer to the array of segregated free lists
+//   - num_lists: Pointer to the number of segregated free lists
+//   - data: Pointer to the allocated memory for the heap
+//   - start_address: The starting address of the heap
+void simple_free(size_t address, ll_list_t *allocated_blocks,
+		 sfl_list_t **lists, size_t *num_lists, void *data,
+		 size_t start_address)
+{
+	// Check if the address is NULL
+	if (address == 0) {
+		return;
+	}
+
+	// Find the block with the given address
+	for (ll_node_t *current_ll = allocated_blocks->head; current_ll;
+	     current_ll = current_ll->next) {
+		if (address == (size_t)((char *)current_ll->data -
+					(char *)data + start_address)) {
+			// Remove the current node from the allocated blocks list
+			if (current_ll->prev) {
+				current_ll->prev->next = current_ll->next;
+			} else {
+				allocated_blocks->head = current_ll->next;
+			}
+
+			// Reconnect the allocated blocks list
+			if (current_ll->next) {
+				current_ll->next->prev = current_ll->prev;
+			}
+
+			// Update the number of allocated blocks
+			allocated_blocks->size -= 1;
+
+			// Add the current node to the segregated free list
+			for (size_t i = 0; i < *num_lists; i++) {
+				if (current_ll->size ==
+				    (*lists)[i].element_size) {
+					// Allocate memory for a new node in the segregated free list
+					sfl_node_t *new_sfl =
+						malloc(sizeof(sfl_node_t));
+					DIE(new_sfl == NULL,
+					    "Malloc failed while allocating node");
+
+					// Set the data of the current node
+					new_sfl->data = current_ll->data;
+
+					// Move to the appropriate position
+					sfl_node_t *previous_sfl =
+						(*lists)[i].head;
+					while (previous_sfl->next != NULL &&
+					       previous_sfl->data <
+						       new_sfl->data) {
+						previous_sfl =
+							previous_sfl->next;
+					}
+
+					// Connect the new node to the last one
+					new_sfl->prev = previous_sfl;
+					new_sfl->next = previous_sfl->next;
+					previous_sfl->next = new_sfl;
+
+					// Update the number of free blocks in the list
+					(*lists)[i].size += 1;
+
+					// Free the memory of the freed node
+					free(current_ll);
+					return;
+				}
+			}
+
+			// If there is no list with the remaining size, add a new list
+
+			// Update the number of lists
+			*num_lists += 1;
+
+			// Reallocate memory for the segregated free lists
+			*lists = realloc(*lists,
+					 *num_lists * sizeof(sfl_list_t));
+
+			// Find the index of the new list
+			for (size_t i = 0; i < *num_lists; i++) {
+				if ((*lists)[i].element_size >
+				    current_ll->size) {
+					// Move the lists to the right
+					for (size_t j = *num_lists - 1; j > i;
+					     j--) {
+						(*lists)[j] = (*lists)[j - 1];
+					}
+
+					// Add and initialise the new list
+					(*lists)[i].element_size =
+						current_ll->size;
+					(*lists)[i].size = 1;
+					(*lists)[i].head =
+						malloc(sizeof(sfl_node_t));
+					(*lists)[i].head->data =
+						current_ll->data;
+					(*lists)[i].head->next = NULL;
+					(*lists)[i].head->prev = NULL;
+
+					// Free the memory of the freed node
+					free(current_ll);
+					return;
+				}
+			}
+		}
+	}
+
+	// If the address is not found, print an error message
+	printf("Invalid free\n");
+}
+
 // Function to dump the memory statistics
+//
 // Parameters:
 //   - num_lists: The number of segregated free lists
 //   - malloc_calls: The number of malloc calls
@@ -322,6 +445,7 @@ void dump_memory(size_t num_lists, size_t malloc_calls, size_t fragmentations,
 }
 
 // Function to free the memory of the heap
+//
 // Parameters:
 //   - segregated_free_lists: The array of segregated free lists
 //   - num_lists: The number of segregated free lists
@@ -368,7 +492,7 @@ int main(void)
 	size_t fragmentations = 0;
 
 	// Initialize the variables for the input
-	size_t start_address, num_lists, bytes_per_list, type, size;
+	size_t start_address, num_lists, bytes_per_list, type, size, address;
 
 	// Allocate memory for the command
 	char command[COMMAND_SIZE];
@@ -379,7 +503,7 @@ int main(void)
 
 		if (!strcmp(command, "INIT_HEAP")) {
 			// Read the parameters for the INIT_HEAP command
-			scanf("%lu %lu %lu %lu", &start_address, &num_lists,
+			scanf("%lx %lu %lu %lu", &start_address, &num_lists,
 			      &bytes_per_list, &type);
 
 			// Initialize the heap
@@ -396,6 +520,13 @@ int main(void)
 
 			malloc_f(size, &list, &num_lists, &allocated_blocks,
 				 &fragmentations, &malloc_calls);
+		} else if (!strcmp(command, "FREE")) {
+			// Read the address of the block to be freed
+			scanf("%lx", &address);
+
+			simple_free(address, &allocated_blocks, &list,
+				    &num_lists, data, start_address);
+			free_calls += 1;
 		} else if (!strcmp(command, "DESTROY_HEAP")) {
 			// Destroy the heap
 			destroy_heap(list, num_lists, data, allocated_blocks);
